8f3c98f953fc5ff9f31457625cd69e6473e74f9e
randamzz <randaa.mzz@yahoo.com>
2025-01-02 20:30:10 +0100

    connexin ith frontend done

--- FLask_API/app.py
+++ FLask_API/app.py
@@ -2,8 +2,11 @@ from flask import Flask, request, jsonify
 import os
 import pandas as pd
 import joblib
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 # Dossier contenant les modèles
 models_directory = 'models'
@@ -57,4 +60,4 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=5001)
